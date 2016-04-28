158c399e5ddeccc580b0efbdbdaa3cf9a8b8c833
Michael Ekstrand <ekstrand@txstate.edu>
2016-04-28 13:04:05 -0500

    simplify codes?

--- rat-functions.rs
+++ rat-functions.rs
@@ -7,8 +7,8 @@ pub use rustr::*;
 use rustr::rmath::lgammafn;
 
 pub fn llbase(alpha: &NumVec, beta: &NumVec) -> RResult<NumVec> {
-  r_message(&format!("alpha rsize: {}", alpha.rsize()));
-  r_message(&format!("beta rsize: {}", beta.rsize()));
+  // r_message(&format!("alpha rsize: {}", alpha.rsize()));
+  // r_message(&format!("beta rsize: {}", beta.rsize()));
 
   let nas = alpha.rsize();
   let nbs = beta.rsize();
@@ -27,10 +27,10 @@ pub fn llbase(alpha: &NumVec, beta: &NumVec) -> RResult<NumVec> {
     unsafe {
       let a = alpha.uat(ax);
       let b = beta.uat(bx);
-      result.uset(i, lgamma(a + b) - lgamma(a) - lgamma(b));
+      result.uset(i, lgammafn(a + b) - lgammafn(a) - lgammafn(b));
     }
   }
-  
+
   Ok(result)
 }
 
@@ -42,29 +42,32 @@ pub fn rats_llbase(alpha: NumVec, beta: NumVec) -> RResult<NumVec> {
 // #[rustr_export]
 pub fn rats_loglike(alpha: NumVec, beta: NumVec, ys: IntVec, ns: IntVec) -> RResult<NumVec> {
   let mut base = try!(llbase(&alpha, &beta));
-  r_message(&format!("ys rsize: {}", ys.rsize()));
-  r_message(&format!("ns rsize: {}", ns.rsize()));
+  // r_message(&format!("ys rsize: {}", ys.rsize()));
+  // r_message(&format!("ns rsize: {}", ns.rsize()));
   let ni = ys.rsize();
   if ni != ns.rsize() {
     return Err(RError::other("mismatched ys & ns arrays"))
   }
   let n = ni as usize;
-  
+
   let np = base.rsize() as usize;
-  
+
+  let max_ai = (alpha.rsize() - 1) as usize;
+  let max_bi = (beta.rsize() - 1) as usize;
+
   for i in 0..np {
     unsafe {
       let mut ll = base.uat(i) * (n as c_double);
-    let ai = min(i, alpha.rsize() as usize);
-    let bi = min(i, beta.rsize() as usize);
-    let a = alpha.uat(ai);
-    let b = beta.uat(bi);
-    for j in 0..n {
-      let yj = ys.uat(j) as c_double;
-      let nj = ns.uat(j) as c_double;
-      ll = ll + lgamma(a + yj) + lgamma(b + nj - yj) - lgamma(a + b + nj);
-    }
-    base.uset(i, ll);
+      let ai = min(i, max_ai);
+      let bi = min(i, max_bi);
+      let a = alpha.uat(ai);
+      let b = beta.uat(bi);
+      for j in 0..n {
+        let yj = ys.uat(j) as c_double;
+        let nj = ns.uat(j) as c_double;
+        ll = ll + lgammafn(a + yj) + lgammafn(b + nj - yj) - lgammafn(a + b + nj);
+      }
+      base.uset(i, ll);
     }
   }
 
